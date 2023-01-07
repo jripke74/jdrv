724d1ea9f71699d623f73aebcaadd8dfd72bdcca
Jeff Ripke <jripke74@gmail.com>
2023-01-07 07:34:53 -0800

    add contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,19 @@
+import { Fragment } from 'react';
+
 const About = () => {
   return (
-    <h1>Contact Page!!!</h1>
-  )
-}
+    <Fragment>
+      <div className='centered'>
+        <h1>How to get ahold of us:</h1>
+        <p>Douglas Ripke</p>
+        <p>509-795-6998</p>
+        <p>douglas@ripke.io</p>
+        <p>Jeff Ripke</p>
+        <p>509-795-6998</p>
+        <p>jeff@ripke.io</p>
+      </div>
+    </Fragment>
+  );
+};
 
 export default About;
