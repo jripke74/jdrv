b0ab8d959ceb03aa8ac20d39ef09a11d939dc5ce
Jeff Ripke <jripke74@gmail.com>
2023-01-07 13:53:17 -0800

    change wording to we

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -15,18 +15,18 @@ const Main = () => {
           Call Jeff Ripke: <a href="tel:509-795-6998">360-202-7984</a>
         </h2>
         <p>
-          Hi, I am Douglas Ripke, a Mobile RV Technician skilled in
+          We are a Mobile RV Technician company skilled in
           refrigerators, AC, Water heaters, Furnace, & Exterior repairs. I am
           certified by National RV Training Academy (NRVTA) and help people with
           RV maintenance, repair, and upgrades. So hit the road with max
           confidence and zero worries. From faulty air conditioners to leaky
-          roofs, I provide you with a quick fix on the road so that you don’t
-          have to cut your trip short. As Mobile RV Tech, I can come to your
+          roofs, we provide you with a quick fix on the road so that you don’t
+          have to cut your trip short. As Mobile RV Tech, we can come to your
           home or wherever your RV is to tune it up for your next outdoor
           adventure.
         </p>
         <p>
-          I live in Spokane Valley, Washington, USA, and am just a call away.
+          We are located in Spokane Valley, Washington, USA, and are just a call away.
           Never hit the road in a faulty RV. Instead, dial the digits and get
           your RV back in tip-top shape.
         </p>
