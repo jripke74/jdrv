6dbfa214f5d8e051661a9e8ac6e7de9f150b49b5
Jeff Ripke <jripke74@gmail.com>
2023-02-11 16:26:18 -0800

    remove h2, add p

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -5,9 +5,9 @@ const Services = () => {
     <div className="services centered">
       <img src={logo} />
       <h1>J&D RV Repair & Inspections</h1>
-      <h2>Certified and Registered Rv Technicians</h2>
+      <p>Certified and Registered Rv Technicians</p>
       <p>We Love RV’ing and we want to help you Love RV’ing also</p>
-      <h2>Services we offer</h2>
+      <p>Services we offer</p>
     </div>
   );
 };
